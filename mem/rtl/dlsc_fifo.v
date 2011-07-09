361d9f96c86fd354838314932362a7fcbce52ffa
Dan Strother <dan@danstrother.com>
2011-07-09 12:40:09 -0700

    created sync FIFO with BRAM support

--- mem/rtl/dlsc_fifo.v
+++ mem/rtl/dlsc_fifo.v
@@ -1,156 +1,169 @@
 module dlsc_fifo #(
     parameter DATA          = 8,    // width of data in FIFO
-    parameter DEPTH         = 16,   // depth of FIFO (doesn't have to be a power-of-2)
-    parameter ALMOST_FULL   = 0,    // assert almost_full when ALMOST_FULL free spaces remain (0 makes it equivalent to full)
-    parameter ALMOST_EMPTY  = 0     // assert almost_empty when ALMOST_EMPTY valid entries remain (0 makes it equivalent to empty)
+    parameter ADDR          = 4,    // depth of FIFO is 2**ADDR
+    parameter ALMOST_FULL   = 0,    // assert almost_full when <= ALMOST_FULL free spaces remain (0 makes it equivalent to full)
+    parameter ALMOST_EMPTY  = 0,    // assert almost_empty when <= ALMOST_EMPTY valid entries remain (0 makes it equivalent to empty)
+    parameter BRAM          = (DATA*(2**ADDR)>=4096) // use block RAM (instead of distributed RAM)
 ) (
     // system
     input   wire                clk,
     input   wire                rst,
     
     // input
-    input   wire                push_en,
-    input   wire    [DATA-1:0]  push_data,
+    input   wire                wr_push,
+    input   wire    [DATA-1:0]  wr_data,
+    output  reg                 wr_full,
+    output  wire                wr_almost_full,
 
     // output
-    input   wire                pop_en,
-    output  wire    [DATA-1:0]  pop_data,
-
-    // status
-    output  reg                 empty,
-    output  reg                 full,
-    output  reg                 almost_empty,
-    output  reg                 almost_full
+    input   wire                rd_pop,
+    output  wire    [DATA-1:0]  rd_data,
+    output  reg                 rd_empty,
+    output  wire                rd_almost_empty
 );
 
 `include "dlsc_synthesis.vh"
-`include "dlsc_clog2.vh"
 
-localparam ADDR     = `dlsc_clog2(DEPTH);
-localparam POWER2   = (DEPTH == (2**ADDR)); // power-of-2 depth can simplify a few things
+localparam DEPTH    = (2**ADDR);
+
 
 // ** storage memory **
-reg [ADDR-1:0] wr_addr;
-reg [ADDR-1:0] rd_addr;
-`DLSC_LUTRAM reg [DATA-1:0] mem[DEPTH-1:0];
 
-assign pop_data = mem[rd_addr];
+reg  [ADDR-1:0] wr_addr;
+wire [ADDR-1:0] rd_addr_next;
+wire            rd_en;
 
-always @(posedge clk) begin
-    if(push_en) begin
-        mem[wr_addr]    <= push_data;
-    end
-end
+generate
+if(!BRAM) begin:GEN_LUTRAM
 
-/* verilator lint_off WIDTH */
+    `DLSC_LUTRAM reg [DATA-1:0] mem[DEPTH-1:0];
 
-// pointer last flags (used for wrapping when DEPTH isn't a power-of-2)
-reg wr_addr_last = 1'b0;
-reg rd_addr_last = 1'b0;
+    reg  [DATA-1:0] mem_rd_data;
+    assign          rd_data         = mem_rd_data;
 
-generate if(!POWER2) begin:GEN_LASTS
     always @(posedge clk) begin
-        if(rst) begin
-            wr_addr_last    <= 1'b0;
-        end else if(push_en) begin
-            wr_addr_last    <= (wr_addr == (DEPTH-2));
+        if(wr_push) begin
+            mem[wr_addr]    <= wr_data;
         end
     end
     always @(posedge clk) begin
-        if(rst) begin
-            rd_addr_last    <= 1'b0;
-        end else if(pop_en) begin
-            rd_addr_last    <= (rd_addr == (DEPTH-2));
+        if(rd_en) begin
+            mem_rd_data     <= mem[rd_addr_next];
         end
     end
-end endgenerate
 
-// write pointer
-wire wr_addr_rst = rst || (wr_addr_last && push_en);
-always @(posedge clk) begin
-    if(wr_addr_rst) begin
-        wr_addr         <= 0;
-    end else if(push_en) begin
-        wr_addr         <= wr_addr + 1;
-    end
+end else begin:GEN_BRAM
+
+    dlsc_ram_dp #(
+        .DATA           ( DATA ),
+        .ADDR           ( ADDR ),
+        .PIPELINE_WR    ( 0 ),
+        .PIPELINE_RD    ( 1 ),
+        .WARNINGS       ( 0 )
+    ) dlsc_ram_dp_inst (
+        .write_clk      ( clk ),
+        .write_en       ( wr_push ),
+        .write_addr     ( wr_addr ),
+        .write_data     ( wr_data ),
+        .read_clk       ( clk ),
+        .read_en        ( rd_en ),
+        .read_addr      ( rd_addr_next ),
+        .read_data      ( rd_data )
+    );
+
 end
-
-// read pointer
-wire rd_addr_rst = rst || (rd_addr_last && pop_en);
-always @(posedge clk) begin
-    if(rd_addr_rst) begin
-        rd_addr         <= 0;
-    end else if(pop_en) begin
-        rd_addr         <= rd_addr + 1;
-    end
-end
+endgenerate
 
 
-// ** control **
-reg [ADDR:0]    cnt;        // 1 extra bit, since we need to store [0,DEPTH] (not just DEPTH-1)
+// ** address generation **
+
+reg  [ADDR-1:0] rd_addr;
+reg  [ADDR-1:0] rd_addr_p1;
+
+assign          rd_addr_next    = rd_pop ? rd_addr_p1 : rd_addr;
 
 always @(posedge clk) begin
     if(rst) begin
+        wr_addr     <= 0;
+        rd_addr     <= 0;
+        rd_addr_p1  <= 1;
+    end else begin
+        if(wr_push) begin
+            wr_addr     <= wr_addr + 1;
+        end
+        if(rd_pop) begin
+            rd_addr     <= rd_addr_p1;
+            rd_addr_p1  <= rd_addr_p1 + 1;
+        end
+    end
+end
 
-        empty           <= 1'b1;
-        full            <= 1'b0;
-        almost_empty    <= 1'b1;
-        almost_full     <= 1'b0;
-        
+
+// ** flags **
+
+reg  [ADDR:0]   cnt;        // 1 extra bit, since we want to store [0,DEPTH] (not just DEPTH-1)
+reg             almost_empty;
+reg             almost_full;
+
+assign          rd_almost_empty = (ALMOST_EMPTY==0) ? rd_empty : almost_empty;
+assign          wr_almost_full  = (ALMOST_FULL ==0) ? wr_full  : almost_full;
+
+always @(posedge clk) begin
+    if(rst) begin
+        wr_full         <= 1'b0;
+        rd_empty        <= 1'b1;
         cnt             <= 0;
-
+        almost_full     <= 1'b0;
+        almost_empty    <= 1'b1;
     end else begin
 
         // pushed; count increments
-        if(push_en && !pop_en) begin
+        if( wr_push && !rd_pop) begin
             cnt             <= cnt + 1;
-            empty           <= 1'b0;                // can't be empty on push
-            full            <= (cnt == (DEPTH-1));  // cnt will be DEPTH (full)
+            wr_full         <= (cnt == (DEPTH-1));  // cnt will be DEPTH (full)
             if(cnt == (      ALMOST_EMPTY  )) almost_empty <= 1'b0;
             if(cnt == (DEPTH-ALMOST_FULL -1)) almost_full  <= 1'b1;
         end
 
         // popped; count decrements
-        if(!push_en && pop_en) begin
+        if(!wr_push &&  rd_pop) begin
             cnt             <= cnt - 1;
-            empty           <= (cnt == 1);          // cnt will be 0 (empty)
-            full            <= 1'b0;                // can't be full on pop
+            wr_full         <= 1'b0;                // can't be full on pop
             if(cnt == (      ALMOST_EMPTY+1)) almost_empty <= 1'b1;
             if(cnt == (DEPTH-ALMOST_FULL   )) almost_full  <= 1'b0;
+        end
+
+        // special empty flag handling..
+        // (since the RAM doesn't support simultaneously reading from the same
+        //  address that is being written to)
+        if(cnt == 1) begin
+            if(rd_pop) begin
+                rd_empty    <= 1'b1;
+            end else begin
+                rd_empty    <= 1'b0;
+            end
         end
 
     end
 end
 
-/* verilator lint_on WIDTH */
+// read on pop, or after first entry is written
+assign          rd_en           = rd_pop || (rd_empty && cnt == 1);
 
+
+// ** simulation checks **
 
 `ifdef DLSC_SIMULATION
 `include "dlsc_sim_top.vh"
 
 always @(posedge clk) begin
 
-    if( push_en && !pop_en && full ) begin
+    if( wr_push && !rd_pop && wr_full ) begin
         `dlsc_error("overflow");
     end
-    if(             pop_en && empty) begin
+    if(             rd_pop && rd_empty) begin
         `dlsc_error("underflow");
     end
-
-//    if(!rst) begin
-//        if(full != (cnt == DEPTH)) begin
-//            `dlsc_error("full flag mismatch");
-//        end
-//        if(empty != (cnt == 0)) begin
-//            `dlsc_error("empty flag mismatch");
-//        end
-//        if(almost_full != (cnt >= (DEPTH-ALMOST_FULL))) begin
-//            `dlsc_error("almost_full flag mismatch");
-//        end
-//        if(almost_empty != (cnt <= (ALMOST_EMPTY))) begin
-//            `dlsc_error("almost_empty flag mismatch");
-//        end
-//    end
 
 end
 
--- mem/tb/dlsc_fifo_tb.sp
+++ mem/tb/dlsc_fifo_tb.sp
@@ -7,10 +7,10 @@
 
 /*AUTOSUBCELL_CLASS*/
 
-#define DATA            8
-#define DEPTH           16
-#define ALMOST_FULL     0
-#define ALMOST_EMPTY    0
+#define DATA            PARAM_DATA
+#define DEPTH           (1<<PARAM_ADDR)
+#define ALMOST_FULL     PARAM_ALMOST_FULL
+#define ALMOST_EMPTY    PARAM_ALMOST_EMPTY
 
 SC_MODULE (__MODULE__) {
 private:
@@ -52,13 +52,13 @@ SP_CTOR_IMP(__MODULE__) : clk("clk",10,SC_NS) /*AUTOINIT*/ {
     SP_AUTO_CTOR;
 
     /*AUTOTIEOFF*/
-    SP_CELL(dut,Vdlsc_fifo);
+    SP_CELL(dut,DLSC_DUT);
         /*AUTOINST*/
 
     rst         = 1;
     read_pct    = 0;
     write_pct   = 0;
-    
+
     SC_METHOD(fifo_method);
         sensitive << clk.negedge_event();
     
@@ -70,68 +70,84 @@ SP_CTOR_IMP(__MODULE__) : clk("clk",10,SC_NS) /*AUTOINIT*/ {
 }
 
 void __MODULE__::fifo_method() {
-    if(rst) {
-        fifo.clear();
-    } else {
-        // pop first
-        if(pop_en) {
-            if(fifo.empty()) {
-                dlsc_error("underflow");
-            } else {
-                fifo.pop_front();
-            }
-        }
-
-        // then push
-        // (to correctly support simultaneous pop and push when full)
-        if(push_en) {
-            if(fifo.size() == DEPTH) {
-                dlsc_error("overflow");
-            } else {
-                fifo.push_back(push_data.read());
-            }
-        }
-
-        // check data
-        if(!fifo.empty()) {
-            dlsc_assert_equals(fifo.front(),pop_data.read());
-        }
-    }
-    
     // don't check at time zero
     if(sc_core::sc_time_stamp() == sc_core::SC_ZERO_TIME) return;
 
-    // check flags
-    dlsc_assert_equals(empty,fifo.empty());
-    dlsc_assert_equals(full,(fifo.size() == DEPTH));
-    dlsc_assert_equals(almost_empty,(fifo.size() <= ALMOST_EMPTY));
-    dlsc_assert_equals(almost_full,(fifo.size() >= (DEPTH-ALMOST_FULL)));
+    if(rst) {
+        // check reset values
+        dlsc_assert_equals(rd_empty,1);
+        dlsc_assert_equals(rd_almost_empty,1);
+        dlsc_assert_equals(wr_full,0);
+        dlsc_assert_equals(wr_almost_full,0);
+        fifo.clear();
+        return;
+    }
+
+    // update model
+    // pop first
+    if(rd_pop) {
+        if(fifo.empty()) {
+            dlsc_error("underflow");
+        } else {
+            fifo.pop_front();
+        }
+    }
+
+    // then push
+    // (to correctly support simultaneous pop and push when full)
+    if(wr_push) {
+        if(fifo.size() == DEPTH) {
+            dlsc_error("overflow");
+        } else {
+            fifo.push_back(wr_data.read());
+        }
+    }
+
+    // check empty flags
+    if(fifo.empty()) {
+        dlsc_assert_equals(rd_empty,1);
+        dlsc_assert_equals(rd_almost_empty,1);
+    } else if(fifo.size() > 1) {
+        dlsc_assert_equals(rd_empty,0);
+        dlsc_assert_equals(rd_almost_empty,(fifo.size() <= ALMOST_EMPTY));
+    } else {
+        // TODO
+    }
+
+    // check full flags
+    dlsc_assert_equals(wr_full,(fifo.size() == DEPTH));
+    dlsc_assert_equals(wr_almost_full,(fifo.size() >= (DEPTH-ALMOST_FULL)));
+
+    // check data
+    if(!fifo.empty() && !rd_empty) {
+        dlsc_assert_equals(rd_data,fifo.front());
+    }
 }
 
 void __MODULE__::stim_method() {
 
     if(rst) {
-        push_en         = 0;
-        push_data       = 0;
-        pop_en          = 0;
+        wr_push         = 0;
+        wr_data         = 0;
+        rd_pop          = 0;
         return;
     }
 
-    bool read_en    = ((rand()%100) >= read_pct ) && !empty;
-    bool write_en   = ((rand()%100) >= write_pct) && (read_en || !full);
+    bool read_en    = ((rand()%100) >= read_pct ) && !rd_empty;
+    bool write_en   = ((rand()%100) >= write_pct) && (read_en || !wr_full);
 
     if(read_en) {
-        pop_en          = 1;
+        rd_pop          = 1;
     } else {
-        pop_en          = 0;
+        rd_pop          = 0;
     }
 
     if(write_en) {
-        push_en         = 1;
-        push_data       = rand() % ((1<<DATA)-1);
+        wr_push         = 1;
+        wr_data         = rand() % ((1<<DATA)-1);
     } else {
-        push_en         = 0;
-        push_data       = 0;
+        wr_push         = 0;
+        wr_data         = 0;
     }
 }
 
@@ -149,7 +165,7 @@ void __MODULE__::stim_thread() {
         wait(clk.negedge_event());
         rst     = 0;
 
-        wait(1,SC_US);
+        wait(10,SC_US);
     }
 
     wait(1,SC_US);
